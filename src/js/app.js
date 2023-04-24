e5bb96cedb0fca643961b6761ca9015ca1334f85
Yevhenii Khyzhniak <keildra258@gmail.com>
2023-04-24 19:14:09 +0300

    second commit

---
+++ src/js/app.js
@@ -0,0 +1,165 @@
+/*
+!(i) 
+Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
+Или когда импортирован весь файл, например import "files/script.js";
+Неиспользуемый (не вызванный) код в итоговый файл не попадает.
+
+Если мы хотим добавить модуль следует его расскоментировать
+*/
+import {
+    isWebp,
+    headerFixed,
+    togglePopupWindows,
+    addTouchClass,
+    addLoadedClass,
+    menuInit,
+} from './modules'
+/* Раскомментировать для использования */
+// import { MousePRLX } from './libs/parallaxMouse'
+
+/* Раскомментировать для использования */
+// import AOS from 'aos'
+
+/* Раскомментировать для использования */
+// import Swiper, { Navigation, Pagination } from 'swiper'
+
+// Включить/выключить FLS (Full Logging System) (в работе)
+window['FLS'] = location.hostname === 'localhost'
+
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML
+! (i) необходимо для корректного отображения webp из css 
+*/
+isWebp()
+/* Добавление класса touch для HTML если браузер мобильный */
+/* Раскомментировать для использования */
+// addTouchClass();
+/* Добавление loaded для HTML после полной загрузки страницы */
+/* Раскомментировать для использования */
+// addLoadedClass();
+/* Модуль для работы с меню (Бургер) */
+/* Раскомментировать для использования */
+// menuInit()
+
+/* Библиотека для анимаций ===============================================================================
+ *  документация: https://michalsnik.github.io/aos
+ */
+// AOS.init();
+// =======================================================================================================
+
+// Паралакс мышей ========================================================================================
+// const mousePrlx = new MousePRLX({})
+// =======================================================================================================
+
+// Фиксированный header ==================================================================================
+// headerFixed()
+// =======================================================================================================
+
+/* Открытие/закрытие модальных окон ======================================================================
+* Чтобы модальное окно открывалось и закрывалось
+* На окно повешай атрибут data-type="<название окна>"
+* И на кнопку, которая вызывает окно так же повешай атрибут data-type="<название окна>"
+
+* На обертку(враппер) окна добавь класс _overlay-bg
+* На кнопку для закрытия окна добавь класс button-close
+*/
+/* Раскомментировать для использования */
+// togglePopupWindows()
+// =======================================================================================================
+// AOS.init({
+//     // Global settings:
+//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//     initClassName: 'aos-init', // class applied after initialization
+//     animatedClassName: 'aos-animate', // class applied on animation
+//     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+//     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//     offset: 120, // offset (in px) from the original trigger point
+//     delay: 0, // values from 0 to 3000, with step 50ms
+//     duration: 400, // values from 0 to 3000, with step 50ms
+//     easing: 'ease', // default easing for AOS animations
+//     once: false, // whether animation should happen only once - while scrolling down
+//     mirror: false, // whether elements should animate out while scrolling past them
+//     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+// });
+
+
+document.querySelector('.js-menu-btn').onclick = function () {
+    console.log(1)
+    this.classList.toggle('actived');
+    document.querySelector('body').classList.toggle('overflow');
+    document.querySelector('.js-header').classList.toggle('show');
+}
+
+
+const openingSwiper = new Swiper('.opening__left-slider', {
+    loop: true,
+    slidesPerView: 3.5,
+    centeredSlides: true,
+    freeMode: true,
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInterction: false
+    },
+    speed: 800
+})
+
+
+const swiper = new Swiper('.recomendation__slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+    },
+    slidesPerView: 3.1,
+    spaceBetwenn: 40,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 2.4,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        375: {
+            slidesPerView: 1,
+        }
+    }
+});
+
+const sellSwiper = new Swiper('.readytosell__wrapper', {
+    loop: true,
+    spaceBetwenn: 100,
+    autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInterction: false,
+    },
+    speed: 800,
+    on: {
+        init() {
+            this.el.addEventListener('mouseenter', () => {
+                this.autoplay.stop();
+            });
+
+            this.el.addEventListener('mouseleave', () => {
+                this.autoplay.start();
+            });
+        }
+    },
+})
+
+
+
+
+
+
+
